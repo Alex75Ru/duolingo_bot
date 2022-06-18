db8c3860c6f116bbb32850570c555be51d1599e4
Anastasia Alekhina <alehinaanastasia581@gmail.com>
2022-06-18 17:47:05 +0300

    Update main.py
    add data_gen() function
    rewrite show_table() function

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import random
 import telebot
 import sqlite3
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from time import sleep
 from conf import xxx
 
 bot = telebot.TeleBot(xxx)
 
 # Создаем файл с БД
-conn = sqlite3.connect("list_users.db", check_same_thread=True)
+conn = sqlite3.connect('list_users.db', check_same_thread=True)
 
 # Создаем курсор который будет делать запросы в БД
 curs = conn.cursor()
@@ -24,8 +23,18 @@ curs = conn.cursor()
 # 6.очки на сайте        2001
 # 7.очки в игре          1
 
-curs.execute(
-    """CREATE TABLE IF NOT EXISTS users(
+
+def connection(fn):
+    def wrapped(*args):
+        conn = sqlite3.connect('list_users.db')
+        curs = conn.cursor()
+        fn(*args)
+        conn.commit()
+        curs.close()
+    return wrapped
+
+
+curs.execute("""CREATE TABLE IF NOT EXISTS users(
    user_id INT PRIMARY KEY,
    first_name TEXT,
    last_name TEXT,
@@ -33,10 +42,10 @@ curs.execute(
    experience_points_start INTEGER,
    experience_points_site INTEGER,
    experience_points_game INTEGER);
-"""
-)
+""")
 conn.commit()
 curs.close()
+conn.close()
 
 
 def add_user(a, b, c, d, e, f, g):
@@ -50,6 +59,7 @@ def add_user(a, b, c, d, e, f, g):
     @param f: INTEGER 6.очки на сайте        2001
     @param g: INTEGER 7.очки в игре          1
     """
+    conn = sqlite3.connect('list_users.db', check_same_thread=True)
     curs = conn.cursor()
     user = (a, b, c, d, e, f, g)
     curs.execute("INSERT INTO users VALUES(?, ?, ?, ?, ?, ?, ?);", user)
@@ -76,7 +86,7 @@ def update_data_user(user_id, e, f, g):
     conn.commit()
     curs.close()
 
-
+    
 # Удаляем пользователя по имени
 def del_user(user_id):
     curs = conn.cursor()
@@ -92,10 +102,10 @@ def result_one(user):
     curs = conn.cursor()
     curs.execute("SELECT * FROM users;")
     one_result = curs.fetchone()
-    return one_result
+    return(one_result)
     curs.close()
 
-
+    
 # запрос данных для первых десяти чел
 def result_ten():
     curs = conn.cursor()
@@ -104,14 +114,15 @@ def result_ten():
     print(ten_results)
     curs.close()
 
-
+    
 # запрос данных обо всех участниках
 def result_all():
+    conn = sqlite3.connect('list_users.db', check_same_thread=True)
     curs = conn.cursor()
     curs.execute("SELECT * FROM users;")
     all_results = curs.fetchall()
-    print(all_results)
     curs.close()
+    return all_results
 
 
 # add_user(213123, 'Raduga', 'Prank You', 'Raduga_prankyou', 300000, 300000, 0)
@@ -120,8 +131,9 @@ def result_all():
 # del_user(213123)
 # print(result_all())
 
-# driver = webdriver.Chrome()
-url = "https://en.duolingo.com/profile/"
+
+#driver = webdriver.Chrome()
+url = 'https://en.duolingo.com/profile/'
 
 
 def parser(base_url, name):
@@ -132,46 +144,43 @@ def parser(base_url, name):
     :param name:
     :return: bool, Total XP
     """
+
     driver = webdriver.Chrome()
     full_url = f"{base_url}{name}"
     driver.get(full_url)
-    if driver.requests[0].response.status_code == http.HTTPStatus.OK:
-        main_page = driver.page_source
-        divs = main_page.split(">")
-        for i in range(len(divs)):
-            if "Total XP" in divs[i]:
-                temp_str = divs[i - 2]
-                total = int(temp_str[: temp_str.find("<") :])
-                print(f"{name}: {total}")
-                return True, total
-    else:
-        print(f"Error: {name}")
-        return False, 0
 
+    main_page = driver.page_source
+    divs = main_page.split(">")
+    for i in range(len(divs)):
+        if "Total XP" in divs[i]:
+            temp_str = divs[i-2]
+            total = int(temp_str[:temp_str.find("<"):])
+            print(f'{name}: {total}')
+            return True, total
 
-@bot.message_handler(commands=["start"])
+    print(f'Error: {name}')
+    return False, 0
+
+      
+@bot.message_handler(commands=['start'])
 def reply_to_start(message):
     """
     Выводит список команд при отправлении команды старт.
-    """
-    commands_dict = {
-        "/start": "Выводит это сообщение",
-        "/take_part #username_в_Duolingo": "Заносит пользователя в таблицу участников",
-        "/show": "Показывает опыт участников",
-        "/show_winner": "Показывает победителя",
-        "/help": "Выводит информацию о боте",
-    }
-    new_line = "\n"
-    bot.send_message(
-        message.chat.id,
-        (
-            "Бот для проведения соревнований в Duolingo.\n\n"
-            f"""{new_line.join(f"{key} : {value}" for key, value in commands_dict.items())}"""
-        ),
-    )
+    """    
+    commands_dict = {"/start": "Выводит это сообщение",
+                     "/take_part #username_в_Duolingo": "Заносит пользователя в таблицу участников",
+                     "/show": "Показывает опыт участников",
+                     "/show_winner": "Показывает победителя",
+                     "/help": "Выводит информацию о боте"
+                     }
+    new_line = '\n'
+    bot.send_message(message.chat.id,
+                     ("Бот для проведения соревнований в Duolingo." 
+                      f'''{new_line.join(f"{key} : {value}" for key, value in commands_dict.items())}''')
+                     )
 
 
-@bot.message_handler(regexp=r"/take_part #\w+")
+@bot.message_handler(regexp=r'/take_part #\w+')
 def reply_to_take_part(message):
     """
     Вводит сообщение от бота в зависимости от того, существует ли введённый им username.
@@ -181,80 +190,83 @@ def reply_to_take_part(message):
     first_name = message.from_user.first_name
     last_name = message.from_user.last_name
     text = message.text
-    sharp_ind = text.find("#")
-    duo_username = text[sharp_ind + 1 : :].strip()
+    sharp_ind = text.find('#')
+    duo_username = text[sharp_ind+1::].strip()
     flag, total = parser(url, duo_username)
-
+    
     if flag:
-        add_user(us_id, first_name, last_name, duo_username, total, 0, 0)
-        bot.reply_to(
-            message,
-            "Отлично! Ты теперь участник соревнования. Разгроми соперников!\U0001F608",
-        )
+        add_user(us_id, first_name, last_name, duo_username, total, 0, 0)    
+        bot.reply_to(message, "Отлично! Ты теперь участник соревнования. Разгроми соперников!\U0001F608")
     else:
-        bot.reply_to(
-            message,
-            (
-                "Такого username не существует. Попробуй ввести команду"
-                "/take_part и свой username в формате /take_part #твой_username_в_Duolingo заново"
-            ),
-        )
+        bot.reply_to(message, ('Такого username не существует. Попробуй ввести команду'
+                               '/take_part и свой username в формате /take_part #твой_username_в_Duolingo заново'))
 
 
-@bot.message_handler(commands=["show"])
+
+def data_gen(data_lst):
+    """
+    Возвращает строку с данными для вывода.
+    data_list - список кортежей с данными.
+    """
+    for data in data_lst:
+        last_name = data[2] if data[2] else ''
+        link = f"<a href='tg://user?id={data[0]}'>{data[1]} {last_name}</a>"
+        data = f"{link}  <b>{data[6]}</b>"        
+        yield data
+
+
+@bot.message_handler(commands=['show'])
 def show_table(message):
     """
     Показывает результаты количество опыта у участников на данный момент.
     """
-    users_result = result_all()
-    new_line = "\n"
-    bot.send_message(
-        message.chat.id,
-        f"""{new_line.join(f"<a href='tg://user?id={data[0]}'>{data[1]} {data[2]}</a>  <b>{data[6]}</b>" for data in users_result)}""",
-        parse_mode="HTML",
-    )
+    new_line = '\n'
+    gen = data_gen(output)
+    msg = bot.send_message(message.chat.id, "Информация загружается")
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.id, text=f'''{new_line.join(f"{data}" for data in gen)}''',
+                    parse_mode='HTML'
+                    )
 
 
-@bot.message_handler(commands=["show_winner"])
+@bot.message_handler(commands=['show_winner'])
 def show_winner(message):
     """
     Выводит имя победителя и картинку для него.
     """
-    pictures_links = (
-        "https://i.pinimg.com/originals/1d/74/90/1d7490d7ca7d03e90a90bc4dcfabc4cc.jpg",
-        "https://img1.fonwall.ru/o/br/harley-quinn-artist-digital-art.jpeg?route=thumb&h=350",
-        "https://img1.fonwall.ru/o/pe/aquaman-superheroes-artist-artwork.jpeg?route=thumb&h=350",
-        "https://img1.fonwall.ru/o/dq/wonder-woman-superheroes-artist-artwork-lnit.jpeg?route=thumb&h=350",
-        "https://i.pinimg.com/736x/b0/7c/f0/b07cf003a2bb9351544f88e364d7787a.jpg",
-    )
-
+    pictures_links = ("https://i.pinimg.com/originals/1d/74/90/1d7490d7ca7d03e90a90bc4dcfabc4cc.jpg",
+                    "https://img1.fonwall.ru/o/br/harley-quinn-artist-digital-art.jpeg?route=thumb&h=350",
+                    "https://img1.fonwall.ru/o/pe/aquaman-superheroes-artist-artwork.jpeg?route=thumb&h=350",
+                    "https://img1.fonwall.ru/o/dq/wonder-woman-superheroes-artist-artwork-lnit.jpeg?route=thumb&h=350",
+                    "https://i.pinimg.com/736x/b0/7c/f0/b07cf003a2bb9351544f88e364d7787a.jpg"
+                   )
+                      
     ind = random.randrange(len(pictures_links))
     bot.send_photo(message.chat.id, pictures_links[ind])
+                     
 
-
-@bot.message_handler(commands=["help"])
+@bot.message_handler(commands=['help'])
 def help(message):
     """
     Выводит информацию о боте в ответ на команду /help.
     """
-    commands_dict = {
-        "/start": "Выводит это сообщение",
-        "/take_part #username_в_Duolingo": "Заносит пользователя в таблицу участников",
-        "/show": "Показывает опыт участников",
-        "/show_winner": "Показывает победителя",
-        "/help": "Выводит информацию о боте",
-    }
-    new_line = "\n"
+    commands_dict = {"/start": "Выводит это сообщение",
+                     "/take_part #username_в_Duolingo": "Заносит пользователя в таблицу участников",
+                     "/show": "Показывает опыт участников",
+                     "/show_winner": "Показывает победителя",
+                     "/help": "Выводит информацию о боте"
+                     }
+    new_line = '\n'
     bot.send_message(
         message.chat.id,
-        ("Бот для проведения соревнований в Duolingo.")(
-            f"""{new_line.join(f"{key} : {value}" for key, value in commands_dict.items())}"""
-        ),
+        (
+            "Бот для проведения соревнований в Duolingo."
+            f'''{new_line.join(f"{key} : {value}" for key, value in commands_dict.items())}'''
+        )
     )
+    
 
-
-while True:
-    try:
-        bot.polling(none_stop=True)
-    except Exception as _ex:
-        sleep(15)
+while True: 
+        try:
+            bot.polling(none_stop=True) 
+        except Exception as _ex:
+            sleep(15)
